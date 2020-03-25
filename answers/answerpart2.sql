ef82668bab3e1bedcfdbf06be44e0f54b78c4ae2
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-03-25 16:22:12 -0400

    part 2 completed

---
+++ answers/answerpart2.sql
@@ -0,0 +1,11 @@
+USE pokemon;
+# What are all the types of pokemon that a pokemon can have?
+SELECT * FROM types;
+# What is the name of the pokemon with id 45?
+SELECT name FROM pokemons WHERE id = 45;
+# How many pokemon are there?
+SELECT COUNT(id) FROM pokemons;
+# How many types are there?
+SELECT COUNT(id) FROM types;
+# How many pokemon have a secondary type?
+SELECT COUNT(*) FROM pokemons WHERE secondary_type IS NOT NULL;
